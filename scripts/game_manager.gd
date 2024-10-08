fffc7babccaa244deb3c054370309d971f84372e
Samuel Mast <samuelmst@gmail.com>
2024-10-08 13:12:24 -0500

    Added Player Damage

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,18 @@
 extends Node
 
+@onready var timer: Timer = $Timer
+
 var score = 0
 
 func add_point():
 	score += 1
 	print("score")
+	
+func reset_game():
+	Engine.time_scale = 0.5
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,11 +6,22 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-var alive = true
+@onready var game_manager: Node = %GameManager
 
-# On player death
+var alive = true
+var hitpoints = 3
+
+# Player takes damage
+func take_damage():
+	hitpoints = hitpoints - 1
+	print(hitpoints)
+	if hitpoints <= 0:
+		die()
+
+# Player death
 func die():
 	alive = false
+	game_manager.reset_game()
 	animation_player.play("death")
 
 func _physics_process(delta: float) -> void:

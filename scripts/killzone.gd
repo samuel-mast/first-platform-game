c5bf70f7bb548b46b0acee131fcb5212c62eeb28
Samuel Mast <samuelmst@gmail.com>
2024-10-08 12:28:03 -0500

    Implemented Smarter Player Death

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,14 +1,12 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-#@onready var player: CharacterBody2D = $"../Player"
-@onready var parent: Node2D = $".."
 
 
 func _on_body_entered(body: Node2D) -> void:
 	Engine.time_scale = 0.5
 	#body.get_node("CollisionShape2D").queue_free()
-	parent.player_death()
+	body.die()
 	timer.start()
 
 func _on_timer_timeout() -> void:

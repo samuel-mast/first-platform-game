eb30779e98ef1ac5c151ec9713e2ddd5de6ead27
Samuel Mast <samuelmst@gmail.com>
2024-10-14 14:00:39 -0500

    Added Basic Pause Menu

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,11 +5,18 @@ extends Node
 @onready var player: CharacterBody2D = $"../Player"
 @onready var HeartClass = preload("res://scenes/heart.tscn")
 @onready var score_panel: Panel = $"../UI/Score"
+@onready var pause_menu: Control = %PauseMenu
 
 var score = 0
+var is_paused = false
 
 func _ready():
 	hearts_container.setMaxHearts(player.MAX_HITPOINTS)
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		update_pause_menu(is_paused)
+		is_paused = !is_paused
 
 func add_point():
 	score += 1
@@ -19,7 +26,14 @@ func reset_game():
 	Engine.time_scale = 0.5
 	timer.start()
 
-
 func _on_timer_timeout() -> void:
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
+
+func update_pause_menu(paused):
+	if paused:
+		pause_menu.hide()
+		get_tree().paused = false
+	else:
+		get_tree().paused = true
+		pause_menu.show()
